8b3d8dc27490690d1da7de410a03fd351d5f492c
rickyzatnika <rickyzatnika91@gmail.com>
2024-08-06 03:40:11 +0700

    fix endpoint barchart

--- src/components/Dashboard/BarChart.jsx
+++ src/components/Dashboard/BarChart.jsx
@@ -21,7 +21,10 @@ const BarChart = () => {
   const [sortData, setSortData] = useState([]);
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, mutate } = useSWR(`${process.env.NEXT_PUBLIC_API_DEV}/api/daftar`, fetcher);
+  // const { data, mutate } = useSWR(`${process.env.NEXT_PUBLIC_API_DEV}/api/daftar`, fetcher);
+
+
+  const { data, mutate } = useSWR(`${process.env.NEXT_PUBLIC_API_PRO}/api/daftar`, fetcher);
 
   useEffect(() => {
     if (data) {
