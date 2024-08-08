cccf8674fb79a4974ae71cc1605f51d5a152c545
rickyzatnika <rickyzatnika91@gmail.com>
2024-08-09 00:30:18 +0700

    changes

--- src/app/dashboard/list-riders/edit/[id]/page.jsx
+++ src/app/dashboard/list-riders/edit/[id]/page.jsx
@@ -140,7 +140,7 @@ const EditRiders = ({ params }) => {
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Biaya Pendaftaran</label>
               <input type="text" value={totalPrice} onChange={(e) => setTotalPrice(e.target.value)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-lime-500 focus:border-lime-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white " />
             </div>
-            <div className="mb-5">
+            <div className="mb-5 flex flex-col gap-3">
               <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Status Pembayaran</label>
               <label className="flex gap-3">
                 <input
