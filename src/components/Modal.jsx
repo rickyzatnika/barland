9d0e0ad279dab69909230eb9dafe1a16b8f266d8
rickyzatnika <rickyzatnika91@gmail.com>
2024-08-10 03:31:39 +0700

    prikitiw

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -11,7 +11,7 @@ const Modal = ({ children, isOpen, handleCancel }) => {
   if (!isOpen) return null;
 
   return (
-    <div className="w-full px-4 h-[100vh] fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm ">
+    <div className="w-full px-4 h-screen fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur ">
       <div className={`shadow-md w-full h-full overflow-y-auto sm:w-4/5 px-4 pb-8 pt-6 rounded-lg ${theme === "light" ? "light" : "dark"}`}>
         <button onClick={handleCancel} className="absolute top-2 text-gray-500 bg-gray-100/90 hover:bg-gray-100 hover:text-gray-600 px-2 rounded-md right-6 text-3xl">&times;</button>
         {children}
